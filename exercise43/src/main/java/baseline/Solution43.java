2b6b9d1180cff1ea982986cd4bdacd4519ca92af
Fed-Abreu <arielsey@hotmail.com>
2021-10-16 18:07:01 -0400

    exercise43 part1

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,37 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution43 {
+    public void createWebsite(){
+        //Make directory = path + Site name
+        //create a new folder = file directory
+    }
+    public void createJAVAFolder(){
+        //Make directory = path + Site name + js
+        //create a new folder = file directory
+    }
+    public void createCSSFolder(){
+        //Make directory = path + Site name + css
+        //create a new folder = file directory
+
+    }
+    public void createHMTL(){
+        //Make directory = path + Site name + index.html
+        //Put content of <title> + siteName + </title> <meta> + author </meta> inside file
+        //create a new file
+    }
+    //Write file and catch exceptions
+        public static void main(String[] args) {
+        //Display Site Name: and Scan String
+        //Display Author: and Scan String
+        //Display Do you want folder for JavaScript? and Scan String
+        //Display Do you want a folder for CSS? Scan String
+        //Display Created "website" and Created: "HTML"
+        //if JAVAFolder = to y then Display Created: "JAVA"
+        //if CSSFolder = to y then Display Created: "CSS"
+
+
+        }
+}
