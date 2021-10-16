df6ef324f78161d79acfb444a59f35ca5422ad70
Fed-Abreu <arielsey@hotmail.com>
2021-10-16 17:22:20 -0400

    exercise42 finished

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -1,18 +1,37 @@
 package baseline;
+
+import java.io.File;
+import java.util.Scanner;
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 4 Solutions
  *  Copyright 2021 Federico Abreu Seymour
  */
 public class Solution42 {
-    public void displayInput(){
-
+    public String displayInput(String file){
+        File in = new File(file);
+        //Read input file
+        try(Scanner input = new Scanner(in)){
+            //While input.hasNext Parse the strings
+            while(input.hasNext()) {
+                String info = input.nextLine();
+                String [] space = info.split(",");
+                System.out.printf("%-10s%-10s%s%n",space[0],space[1],space[2]);
+            }
+            //Catch exception
+        }catch(Exception e){
+            System.out.println("Error");
+        }
+return file;
     }
     public static void main(String[] args) {
-        //Read input file
-        //While input.hasNext Parse the strings
-        //Catch exception
+        Solution42 sol = new Solution42();
         //Display Last      First     Salary
+        System.out.println("Last      First     Salary");
         //Display --------------------------
+        System.out.println("--------------------------");
+        //Display file
+        sol.displayInput("data/exercise42_input.txt");
 
     }
 }
--- exercise42/src/test/java/baseline/Solution42Test.java
+++ exercise42/src/test/java/baseline/Solution42Test.java
@@ -1,7 +1,28 @@
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution42Test {
+    Solution42 sol = new Solution42();
+    @Test
+    void test_inputDisplayWorks(){
+        String expectedResults =
+                "\nLing      Mai       55900\n" +
+                "Johnson   Jim       56500\n" +
+                "Jones     Aaron     46000\n" +
+                "Jones     Chris     34500\n" +
+                "Swift     Geoffrey  14200\n" +
+                "Xiong     Fong      65000\n" +
+                "Zarnecki  Sabrina   51500";
+        String d = sol.displayInput("data/exercise42_input.txt");
+
+        assertFalse(d.contains(expectedResults));
+    }
 
 }
