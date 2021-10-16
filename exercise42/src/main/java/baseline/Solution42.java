cfcc63a0888663eebdcda5b4ffde6ba03d927a0a
Fed-Abreu <arielsey@hotmail.com>
2021-10-16 16:45:29 -0400

    exercise42 part1

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,18 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution42 {
+    public void displayInput(){
+
+    }
+    public static void main(String[] args) {
+        //Read input file
+        //While input.hasNext Parse the strings
+        //Catch exception
+        //Display Last      First     Salary
+        //Display --------------------------
+
+    }
+}
