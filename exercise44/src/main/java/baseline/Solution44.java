4a0b4932c4183a4f0cf319cb737838a515703de7
Fed-Abreu <arielsey@hotmail.com>
2021-10-17 17:33:14 -0400

    exercise44 finished

--- exercise44/src/main/java/baseline/Solution44.java
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -1,23 +1,69 @@
 package baseline;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 4 Solutions
  *  Copyright 2021 Federico Abreu Seymour
  */
 public class Solution44 {
-    public void fileInput(){
+    //Couldn't figure out how to compile using fileInput method so got rid of it
 
-    }
-    public void fileOutput(){
+    public int fileOutput(){
+        JSONParser parser = new JSONParser();
+        // While loop if the value is valid or not
+        int flag = 0;
+        while (flag == 0) {
+            try {
+                //Read json file using parse and store it in a object
+                Object obj = parser.parse(new FileReader("data/exercise44_input.json"));
 
+                JSONObject jsonObject = (JSONObject) obj;
+
+                // Read products as an Array
+                JSONArray subjects = (JSONArray) jsonObject.get("products");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+                //Display What is the product name and Scan
+                System.out.print("What is the product name? ");
+                String input = bufferedReader.readLine();
+
+                // Loop through an iterator and get value of name and determine if matches
+                for (Object subject : subjects) {
+                    JSONObject json = (JSONObject) subject;
+
+                    //Compare it with given input
+                    String name = (String) json.get("name");
+                    if (input.equals(name.toLowerCase())) {
+                        System.out.println("Name: " + name);
+                        System.out.println("Price: " + json.get("price"));
+                        System.out.println("Quantity: " + json.get("quantity"));
+                        flag = 1;
+                    }
+                }
+                //If not a valid input Display Sorry, that product was not found in our inventory
+                if (flag == 0) {
+                    System.out.println("Sorry, that product was not found in our inventory");
+                }
+                //And catch exceptions
+            } catch (Exception e) {
+               System.out.println("Error");
+            }
+        }
+
+
+        return flag;
     }
-    public static void main(String[] args) {
-        //Read json file using parse and store it in a object
-        //Read products as an Array
-        //While loop if the value is valid or not
-        //Display What is the product name? and Scan
-        //Loop through an iterator and get value of name and determine if matches
-        //If not a valid input Display Sorry, that product was not found in our inventory
-        //And catch exceptions
+    public static void main(String[] args){
+        Solution44 sol = new Solution44();
+
+        sol.fileOutput();
+
 
     }
 }
--- exercise44/src/test/java/baseline/Solution44Test.java
+++ exercise44/src/test/java/baseline/Solution44Test.java
@@ -1,7 +1,26 @@
 package baseline;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution44Test {
+
+    JSONParser parser = new JSONParser();
+    @Test
+    void test_CanDisplay() throws IOException, ParseException {
+        Object obj = parser.parse(new FileReader("data/exercise44_input.json"));
+
+        String actualResults = "products";
+
+        assertNotEquals(obj,"{ products : [ {name: Widget, price: 25.00, quantity: 5 }, {name: Thing, price: 15.00, quantity: 5 }, {name: Doodad, price: 5.00, quantity: 10 }]}");
+    }
 
 }
