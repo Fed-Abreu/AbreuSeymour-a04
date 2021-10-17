03b4004f6fdb5e7733cb2a1a5c96eaa89e7da269
Fed-Abreu <arielsey@hotmail.com>
2021-10-17 16:34:04 -0400

    exercise44 part1

---
+++ exercise43/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+Q
+!data/website/awesomeco/index.html,c\5\c5c5490a434621c8562edd261234bed68a4c3065
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+G
+docs/Solution43UML.puml,9\3\93db8922d61750db653b0177c797aa9fd74e4df0
+V
+&src/main/java/baseline/Solution43.java,b\4\b4347542199e6f8f4a0c2a06abf7605a025d7459
+Z
+*src/test/java/baseline/Solution43Test.java,f\f\ffe32549da85cbb99a78b340561cc4c6999961c2
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,23 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution44 {
+    public void fileInput(){
+
+    }
+    public void fileOutput(){
+
+    }
+    public static void main(String[] args) {
+        //Read json file using parse and store it in a object
+        //Read products as an Array
+        //While loop if the value is valid or not
+        //Display What is the product name? and Scan
+        //Loop through an iterator and get value of name and determine if matches
+        //If not a valid input Display Sorry, that product was not found in our inventory
+        //And catch exceptions
+
+    }
+}
