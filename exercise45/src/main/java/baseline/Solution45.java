ef5fe65ac8a2c8298923ec2025f60c6eb5b2f747
Fed-Abreu <arielsey@hotmail.com>
2021-10-17 17:54:06 -0400

    exercise45 part1

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,18 @@
+package baseline;
+
+public class Solution45 {
+    public void readFile(){
+
+    }
+    public void writeFile(){
+
+    }
+    public static void main(String[] args) {
+        //Display What is the name of the output file: and Scan String
+        //read the file
+        //Make a list of the file and write the files to output
+        //Using while loop read file lines and store them into a list
+        //Write output using for loop and replace 'utilize' to 'use'
+
+    }
+}
