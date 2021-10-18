f430f39dadda2c55b294c0cecfd5c8fd3302f8cd
Fed-Abreu <arielsey@hotmail.com>
2021-10-17 20:47:36 -0400

    exercise46 part1

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,15 @@
+package baseline;
+
+public class Solution46 {
+    public static void main(String[] args) {
+        //Get map and read files
+        //Get path to file and variables
+        //read file with a while loop and spilt white spaces up
+        //Then for loop length to make key = array
+        //If map contains key then make a increase and equal to
+        //Return the map
+        //For String, Integer : map make key = to entry and values incerease to entry
+        //Sort array to value and store
+        //Display the Histogram
+    }
+}
